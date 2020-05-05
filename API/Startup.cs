12fc42e99653d48929e97d69318f20d53607974f
Jared Emrich <jared.emrich@bluefolder.com>
2020-05-04 21:10:11 -0600

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,7 +31,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
-            
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
 
         }
@@ -45,6 +51,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
